b79080113f99a4119e8a6f8439b88b9899687318
X7md <x7mdNet@gmail.com>
2023-05-25 16:32:28 +0300

    add `auto complete`

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
+import './style.css';
+import { tags } from '@lezer/highlight';
 import { minimalSetup, EditorView } from 'codemirror';
 import { sql } from '@codemirror/lang-sql';
 import { EditorState, Compartment } from '@codemirror/state';
-import { autocompletion } from '@codemirror/autocomplete';
+import { acceptCompletion, autocompletion } from '@codemirror/autocomplete';
+import { keymap } from '@codemirror/view';
+
 const languageConf = new Compartment();
 
 new EditorView({
   doc: 'select ',
-  extensions: [minimalSetup, languageConf.of(sql()), autocompletion({})],
-  parent: document.querySelector('#app'),
+  extensions: [
+    minimalSetup,
+    keymap.of([{ key: 'Tab', run: acceptCompletion }]),
+    languageConf.of(sql()),
+    autocompletion({}),
+  ],
+  parent: document.querySelector('#code-text'),
 });
